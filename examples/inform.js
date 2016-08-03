904074cdc052c4853b7b78746d7e81cc40476177
Mike <ellokopoko@gmail.com>
2016-08-03 21:54:46 +0300

    add inform

---
+++ examples/inform.js
@@ -0,0 +1,18 @@
+var events = document.getElementById("events"),
+    log = events.getElementsByClassName("events-log")[0],
+    buttonClear = events.getElementsByClassName("clear-log")[0];
+
+function inform( message ) {
+    var li = document.createElement("LI");
+    li.innerHTML = message;
+    log.appendChild(li);
+}
+
+inform.clear = function() {
+    log.innerHTML = "";
+}
+
+buttonClear
+    .addEventListener("click", function(e) {
+        inform.clear();
+    });
